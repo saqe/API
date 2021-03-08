5159313bb1c828ecc9dc90c82189e83514484606
Saqib <18554574+saqe@users.noreply.github.com>
2021-03-08 17:15:26 +0500

    Added teacherCollection and findTeacher function

--- db/mongodb.py
+++ db/mongodb.py
@@ -5,10 +5,18 @@ class MongoDB:
     def __init__(self,):
         self.client = pymongo.MongoClient(getenv('MONGODB_URI'))
         self.studentCollection  = self.client.User['Student']
+        self.TeacherCollection  = self.client.User['Teacher']
         self.scheduleCollection = self.client.Classes['Schedule']
 
     def findStudent(self, firebase_id, parameters):
         response = self.studentCollection.find_one(
+            {'firebase_user_id':firebase_id},
+            parameters
+        )
+        return response
+    
+    def findTeacher(self, firebase_id, parameters):
+        response = self.TeacherCollection.find_one(
             {'firebase_user_id':firebase_id},
             parameters
         )
